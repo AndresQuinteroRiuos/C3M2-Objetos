94f2f1a5e0aef1c65490b171b1a849c41ed37ff1
Andres Mauricio Quintero Rios <Quintero2273@hotmail.com>
2025-08-08 15:39:35 -0500

    inicia el repo de objetos

---
+++ src/Vehiculo.java
@@ -0,0 +1,9 @@
+public interface Vehiculo {
+
+    void moverse(); // Metodo abstracto
+
+    int obtenerVelocidad(); // Metodo abstracto
+
+    void cambiarMarcha(int cambio); // Metodo abstracto
+
+}
